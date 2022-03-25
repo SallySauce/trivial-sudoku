d4cee8ec6b883d2ce30b33fead520daee9567894
Sorry01 <2647293890@qq.com>
2022-03-25 09:28:50 +0800

    [script] fun bash exercise

--- bench.sh
+++ bench.sh
@@ -3,44 +3,53 @@
 _err() { echo -e "$0: " "$@" >&2 ; exit 1; }
 
 N=50
-
 ITER=10
 TIMEOUT=9999999
 
 while [[ $# -gt 0 ]]; do case "$1" in
-    -iter=*)       ITER=${1#*=}; shift ;;
+    -iter=*)       ITER=${1#*=};    shift ;;
     -timeout=*)    TIMEOUT=${1#*=}; shift ;;
-    -N=*)          N=${1#*=}; shift ;;
+    -N=*)          N=${1#*=};       shift ;;
     -*)            _err "未知选项: $1" ;;
 esac; done
 
 IN=tests/test$N
 OUT=tests/answer$N
 
-TMP=$(mktemp)
-TOTAL_TIME=0.0
+_run_benchmark() {
+    local ITER=$1
+    local IN=$2
+    local OUT=$3
 
-for i in $(seq $ITER); do
+    TMP=$(mktemp)
+    TOTAL_TIME=0.0
 
-    WRONG_LINES=$({ time timeout $TIMEOUT diff -ab $OUT <(echo $IN | ./sudoku_solve) | wc -l; } 2>$TMP)
-    r=$?
-    [ $r -eq 124 ] && echo "Timeout" && exit
-    [ $r -eq 2 ]   && echo "Not exists" && exit
-    [ $r -ne 0 ]   && echo "sudoku_solve exited with non-zero code" && exit
-    [ $WRONG_LINES -ne 0 ] && echo "answer is wrong" && exit
+    for i in $(seq $ITER); do
 
-    TIME=$(cat $TMP | head -2 | tail -1 | awk -F' ' '{print $2}' | awk 'match($0, /([0-9]+)m([.0-9]+)s/, a) {print 60 * a[1] + a[2]}')
+        WRONG_LINES=$({ time diff -ab $OUT <(echo $IN | ./sudoku_solve) | wc -l; } 2>$TMP)
+        r=$?
+        [ $r -eq 2 ]   && echo "Not exists" && exit
+        [ $r -ne 0 ]   && echo "sudoku_solve exited with non-zero code" && exit
+        [ $WRONG_LINES -ne 0 ] && echo "answer is wrong" && exit
 
-    TOTAL_TIME=$(echo $TOTAL_TIME $TIME | awk '{print $1 + $2}')
+        TIME=$(cat $TMP | head -2 | tail -1 | gawk -F' ' '{print $2}' | gawk 'match($0, /([0-9]+)m([.0-9]+)s/, a) {print 60 * a[1] + a[2]}')
 
-    echo -n '.'
+        TOTAL_TIME=$(echo $TOTAL_TIME $TIME | gawk '{print $1 + $2}')
 
-done;
+        echo -n '.'
 
-echo ''
+    done;
 
-AVG_TIME=$(echo $TOTAL_TIME $ITER | awk '{print 1000 * $1 / $2}')
+    echo ''
 
-rm $TMP
+    AVG_TIME=$(echo $TOTAL_TIME $ITER | gawk '{print 1000 * $1 / $2}')
 
-echo -e "\033[1;32m通过测试!!! 平均耗时 $AVG_TIME ms\033[0m"
+    rm $TMP
+
+    echo -e "\033[1;32m通过测试!!! 平均耗时 $AVG_TIME ms\033[0m"
+}
+
+
+export -f _run_benchmark
+timeout $TIMEOUT bash -c "_run_benchmark $ITER $IN $OUT"
+[ $? -eq 124 ] && echo "" && _err "运行时间超过 $TIMEOUT 秒，自动退出"
